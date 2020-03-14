7eaaac94df9617d18053814d35b0514db2ad9e40
David-Project09 <54827654+David-Project09@users.noreply.github.com>
2020-03-13 19:14:09 -0600

    Add files via upload

---
+++ tarea 4 triangulo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "insert number x";
+    cin >> x;
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            cout << "*";
+        cout << endl;
+    }
+    cout << "isoceles triangle ";
+    
+    
+
+    
+    
+    return(0);
+
+
+
+}
